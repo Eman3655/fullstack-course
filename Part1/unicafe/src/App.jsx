7dd33fd975254fb92ba3235d0fbbbb5eed694b70
Eman3655 <emmoabdelhamid2021@gmail.com>
2025-11-20 13:11:40 +0200

    add Ex 1.7/part 1

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -7,6 +7,7 @@ const Button = (props) => (
 )
 
 const Display = props => <div>{props.text} {props.value}</div>
+const Total = props => <div>{props.text} {props.value}</div>
 
 const App = () => {
   const [good, setGood] = useState(0)
@@ -19,10 +20,13 @@ const App = () => {
       <Button onClick={() => setGood(good + 1)} text="good" />
       <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button onClick={() => setBad(bad + 1)} text="bad" />
-        <h1>statistics</h1>
+      <h1>statistics</h1>
       <Display value={good} text="good" />
       <Display value={neutral} text="neutral" />
       <Display value={bad} text="bad" />
+      <Total value={good + neutral + bad} text="all" />
+      <Total value={(good - bad) / (good + neutral + bad)} text="average" />
+      <Total value={good / (good + neutral + bad) * 100 + " %"} text="positive" />
     </div>
   )
 }
