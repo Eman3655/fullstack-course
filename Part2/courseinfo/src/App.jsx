b5c473409b06302e4063ffdd8b95fab50957cef5
Eman3655 <emmoabdelhamid2021@gmail.com>
2025-11-22 14:23:10 +0200

    add Ex 2.5/part2

--- Part2/courseinfo/src/App.jsx
+++ Part2/courseinfo/src/App.jsx
@@ -1,28 +1,4 @@
-const Header = (props) => <h1>{props.course}</h1>
-
-const Part = (props) => (
-  <p>
-    {props.part.name} {props.part.exercises}
-  </p>
-)
-
-const Content = (props) => (
-  <div>
-    {props.parts.map(part =><Part key={part.id} part={part} /> )}
-  </div>
-)
-
-const Total = (props) => <h3>total of {props.total} exercises</h3>
-
-const Course = (props) => {
-  return (
-    <div>
-      <Header course={props.course.name} />
-      <Content parts={props.course.parts} />
-      <Total total={props.course.parts.reduce((sum, part) => sum + part.exercises,0)} />
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
     const courses = [
