aa1816e4b9e21818fef9b43b78a492c61578be28
Eman3655 <emmoabdelhamid2021@gmail.com>
2025-11-29 15:03:20 +0200

    add Ex 2.17/part2

--- Part2/Phonebook/src/App.jsx
+++ Part2/Phonebook/src/App.jsx
@@ -5,7 +5,6 @@ import Persons from './components/Persons'
 import personsService from './components/personServ'
 import Notification from './components/Notification'
 
-
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
@@ -26,27 +25,39 @@ const addPerson = async (event) => {
   event.preventDefault()
 
   const existingPerson = persons.find(person => person.name === newName)
-  if (existingPerson) {
-    const ok = window.confirm(
-      `${newName} is already added to phonebook, replace the old number with a new one?`
-    )
-    if (!ok) return
+if (existingPerson) {
+  const ok = window.confirm(
+    `${newName} is already added to phonebook, replace the old number with a new one?`
+  )
+  if (!ok) return
 
-    const changedPerson = { ...existingPerson, number: newNumber }
+  const changedPerson = { ...existingPerson, number: newNumber }
 
-    personsService
-      .update(existingPerson.id, changedPerson)
-      .then(returnedPerson => {
-        setPersons(
-          persons.map(p => p.id === existingPerson.id ? returnedPerson : p)
-        )
-        setNewName('')
-        setNewNumber('')
-        setNotification(`Updated number of ${returnedPerson.name}`)
-        setTimeout(() => {
-          setNotification(null)
-        }, 5000)
+  personsService
+    .update(existingPerson.id, changedPerson)
+    .then(returnedPerson => {     
+      setPersons(
+        persons.map(p => p.id === existingPerson.id ? returnedPerson : p)
+      )
+      setNewName('')
+      setNewNumber('')
+      setNotification({
+        text: `Updated number of ${returnedPerson.name}`,
+        type: 'notification'
       })
+      setTimeout(() => setNotification(null), 5000)
+    })
+    .catch(error => {
+      console.error('Error updating person:', error)
+
+      setNotification({
+        text: `Information of ${existingPerson.name} has already been removed from server`,
+        type: 'error' 
+      })
+      setTimeout(() => setNotification(null), 5000)
+
+      setPersons(persons.filter(p => p.id !== existingPerson.id))
+    })
     return
   }
   const newPerson = { name: newName, number: newNumber }
@@ -57,11 +68,14 @@ const addPerson = async (event) => {
       setPersons(persons.concat(created))
       setNewName('')
       setNewNumber('')
-      setNotification(`Added ${created.name}`)
-      setTimeout(() => {
-        setNotification(null)
-      }, 5000)
+       setNotification({
+      text: `Added ${created.name}`,
+      type: 'notification'     
     })
+    setTimeout(() => {
+      setNotification(null)
+    }, 5000)
+  })
     .catch(error => {
       console.error("Error adding person:", error)
     })
@@ -88,7 +102,7 @@ const addPerson = async (event) => {
         <div>
       <h2>Phonebook</h2>
 
-      <Notification message={notification} />
+      <Notification notification={notification} />
 
       <PersonsFilter
         filter={filter}
--- Part2/Phonebook/src/components/Notification.jsx
+++ Part2/Phonebook/src/components/Notification.jsx
@@ -1,13 +1,14 @@
-const Notification = ({ message }) => {
-  if (message === null) {
+const Notification = ({ notification }) => {
+  if (notification === null) {
     return null
   }
 
   return (
-    <div className="notification">
-      {message}
+    <div className={notification.type}>
+      {notification.text}
     </div>
   )
 }
 
 export default Notification
+
