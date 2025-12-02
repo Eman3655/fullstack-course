e30271824223a546308017e687bd590ffcc58914
Eman3655 <emmoabdelhamid2021@gmail.com>
2025-12-02 13:40:30 +0200

    add Ex 3.4/part3

--- Part3/backend/index.js
+++ Part3/backend/index.js
@@ -1,19 +1,21 @@
 const express = require('express')
 const app = express()
 
-const persons = [
+const rawPersons = [
   { id: '1', name: 'Arto Hellas', number: '040-123456' },
   { id: '2', name: 'Ada Lovelace', number: '39-44-5323523' },
   { id: '3', name: 'Dan Abramov', number: '12-43-234345' },
   { id: '4', name: 'Mary Poppendieck', number: '39-23-6423122' }
 ]
 
+const persons = new Map(rawPersons.map(p => [p.id, p]))
+
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  res.json(Array.from(persons.values()))
 })
 
 app.get('/info', (req, res) => {
-  const count = persons.length
+  const count = persons.size
   const date = new Date()
   res.send(`
     <p>Phonebook has info for ${count} people</p>
@@ -23,8 +25,7 @@ app.get('/info', (req, res) => {
 
 app.get('/api/persons/:id', (req, res) => {
   const id = req.params.id
-  const person = persons.find(p => p.id === id)
-
+  const person = persons.get(id)
   if (person) {
     res.json(person)
   } else {
@@ -32,8 +33,25 @@ app.get('/api/persons/:id', (req, res) => {
   }
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+  const { id } = req.params
+  if (!persons.has(id)) {
+    return res.status(404).json({ error: 'Person not found' })
+  }
+  persons.delete(id)
+  res.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+
+
+
+
+
+
+
 
